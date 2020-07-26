a48f88254dabd96394ed5ec4edf2d0f404711c1a
BeomJobs <bsjo@teamblind.com>
2020-07-26 19:05:17 +0900

    window_time

---
+++ test/transformers/window_time_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rxdart/rxdart.dart';
+
+/**
+ * WindowCount
+ * 
+ * 각 항목이 Stream소스 시퀀스의 항목을 포함 하는 스트림을 생성하고
+ * 주어진시간마다 샘플링하여 창을 내보냅니다.
+ */
+
+Stream<int> getStream(int n) async* {
+  var k = 0;
+
+  while (k < n) {
+    await Future<Null>.delayed(const Duration(milliseconds: 100));
+
+    yield k++;
+  }
+}
+
+// time    0ms    100ms    150ms    200ms    250ms    300ms    350ms    400ms    450ms    500ms    550ms    600ms
+// source |       0                 1                 2                 3                 4
+// window |       0                              1                         2                                     3
+
+main() {
+  test('windowTime', () async {
+    // given
+    var a = getStream(4);
+
+    // when
+    Stream<List<int>> result = a
+        .windowTime(Duration(milliseconds: 160))
+        .asyncMap((stream) => stream.toList());
+
+    // then
+    await expectLater(
+        result,
+        emitsInOrder(<dynamic>[
+          const [0, 1],
+          const [2, 3],
+          emitsDone
+        ]));
+  }, timeout: Timeout(Duration(seconds: 5)));
+}
